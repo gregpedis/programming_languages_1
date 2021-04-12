584dfbfa35d6fd1d81e0a3c08343d00f0cc4ae67
kenji <trafalgar1618@hotmail.com>
2021-04-12 14:09:42 +0300

    Finished the ocaml implementation for project_1 part_2

--- project_1/part_2/ocaml_testbed/helloworld.ml
+++ project_1/part_2/ocaml_testbed/helloworld.ml
@@ -22,8 +22,6 @@ let get_direction c =
   | 'R' -> Right
   | _ -> raise Invalid_Direction
 
-let get_room x y c = { x=x; y=y; dir=get_direction c }
-
 (* explodes a string to a list of 'a by applying f to each char *)
 let explode_string s f = 
   let idx_to_a idx = f (String.get s idx) in
@@ -32,7 +30,7 @@ let explode_string s f =
 
 (* ARGUMENT PARSING *)
 let get_dimensions line = 
-  let first_int vals = int_of_string (first vals)
+  let first_int vals = int_of_string (List.hd vals)
   and second_int vals = int_of_string (second vals)
   in
     let parse_args args = first_int args, second_int args 
@@ -64,7 +62,7 @@ let get_inputVals fn =
 (* ALGORITHMS *)
 let find_matches rooms d = 
   let is_valid room = room.dir == d in
-    List.filter is_valid rooms
+    List.map (fun x -> Some x) (List.filter is_valid rooms)
 
 let get_valid_perimeter_rooms rooms =
   let row_first = List.hd rooms
@@ -77,7 +75,26 @@ let get_valid_perimeter_rooms rooms =
     @ find_matches column_first Left 
     @ find_matches column_last Right
   
-let get_valid_count_by_room rooms room = 5
+let get_room rooms x y dir = 
+  let inside_x = x >= 0 && x < List.length rooms
+  and inside_y = y >= 0 && y< List.length (List.hd rooms)
+  in
+    if inside_x && inside_y then
+      let room = (List.nth (List.nth rooms x) y) in
+        if room.dir == dir then Some room else None
+    else None
+
+let rec get_valid_count_by_room rooms curr_room =
+  match curr_room with
+  | Some room -> begin
+      let room_up    = get_room rooms (room.x-1) room.y Down
+      and room_down  = get_room rooms (room.x+1) room.y Up
+      and room_left  = get_room rooms room.x (room.y-1) Right
+      and room_right = get_room rooms room.x (room.y+1) Left
+      and f r = get_valid_count_by_room rooms r
+      in 1 + f room_up + f room_down + f room_left + f room_right
+    end
+  | None -> 0
 
 let solve iv = 
   let total_room_count = iv.size_x * iv.size_y
