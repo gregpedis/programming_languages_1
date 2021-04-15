ce739096faac564f71aface0452e084477780286
kenji <trafalgar1618@hotmail.com>
2021-04-15 11:17:29 +0300

    Finished the ocaml implementation of project 1 part 1

--- project_1/part_1/ocaml_testbed/helloworld.ml
+++ project_1/part_1/ocaml_testbed/helloworld.ml
@@ -1,91 +1,31 @@
-(* TESTBED PARAMETERS *)
-let line1 = "11 3"
-let line2 = "42 -10 8 1 11 -6 -12 16 -15 -11 13"
-let length = 11
-let hospital_count = 3
-
-
 (* LIBRARY FUNCTIONS *)
-let first lst = List.nth lst 0
-let second lst = List.nth lst 1
 let last lst = List.nth lst (List.length lst - 1)
-
 type inputVals = { hc:int; dc:int; days:int list }
-
-
-(* ARGUMENT PARSING *)
-let readfile fn =
-  let ic = open_in fn in
-    try
-      let l1 = input_line ic
-      and l2 = input_line ic
-      in
-        flush stdout;
-        close_in ic;
-        l1,l2
-    with e ->
-      close_in_noerr ic;
-      raise e
-
-let get_params line = 
-  let first_int vals = int_of_string (first vals)
-  and second_int vals = int_of_string (second vals)
-  in
-    let parse_args args = first_int args, second_int args 
-    and values = String.split_on_char ' ' line
-    in
-      parse_args values
-
-let get_days line = 
-  let values = String.split_on_char ' ' line
-  in
-    List.map int_of_string values
-
-let get_inputVals = 
-let filename = Sys.argv.(1)
-  in
-    let l1,l2 = readfile filename
-    in 
-      let dc, hc = get_params l1
-      and days = get_days l2
-      in
-        { hc = hc; dc = dc; days = days }
 
 
 (* ALGORITHMS *)        
 let get_simplified_prefixes l hc =
   let simplify v = -1 * (v + hc) in
-    let rec inner_prefixes l last_sum res =
-      match l with
-      | [] -> []
-      | [a] ->  res @ [ simplify a + last_sum ]
-      | hd::tl -> 
-        let new_sum = simplify hd + last_sum in
-          let new_res = res @ [ new_sum ]
-          in
-            inner_prefixes tl new_sum new_res
-            in
-              0 :: inner_prefixes l 0 [] 
-
-
-let some f acc v =
-  match acc with
-  | [] -> [v]
-  | [a] -> acc @ [f a v]
-  | _:: tl -> acc @ [f (last tl) v]
-
-
+    let partial_apply acc v =
+      match acc with 
+      | [] -> [simplify v]
+      | [a] 
+      | a :: _ -> (simplify v + a) :: acc
+    in
+      0 :: List.rev (List.fold_left partial_apply [] l)
+      
 let solve prefixes =
   if last prefixes >= 0 then (List.length prefixes - 1)
   else 
-    let partial_apply f acc v = 
+    let apply_left f acc v = 
       match acc with
       | [] -> [v]
-      | [a] -> acc @ [f a v]
-      | _:: tl -> acc @ [f (last tl) v]
-    in
-      let valid_left = List.fold_left (partial_apply min) [] prefixes 
-      and valid_right = List.rev (List.fold_left (partial_apply max) [] (List.rev prefixes))
+      | [a] 
+      | a :: _ -> (f a v) :: acc 
+    in 
+      let apply_right f v acc = apply_left f acc v in
+      let valid_left = List.rev (List.fold_left (apply_left min) [] prefixes )
+      and valid_right = List.fold_right (apply_right max) prefixes []
       in
         let rec rec_solve valid_left valid_right i j res =
           match (valid_left, valid_right) with
@@ -107,10 +47,45 @@ let solve prefixes =
           (rec_solve valid_left valid_right 0 0 (-1)) 
 
 
+(* ARGUMENT PARSING *)
+let readfile fn =
+  let ic = open_in fn in
+    try
+      let l1 = input_line ic
+      and l2 = input_line ic
+      in
+        flush stdout;
+        close_in ic;
+        l1,l2
+    with e ->
+      close_in_noerr ic;
+      raise e
+
+let get_params line = 
+  let first_int vals = int_of_string (List.hd vals)
+  and second_int vals = int_of_string (List.nth vals 1)
+  in
+    let parse_args args = first_int args, second_int args 
+    and values = String.split_on_char ' ' line
+    in
+      parse_args values
+
+let get_days line = 
+  let values = String.split_on_char ' ' line
+  in
+    List.map int_of_string values
+
+let get_inputVals = 
+  let filename = Sys.argv.(1) in
+  let l1,l2 = readfile filename in
+    let dc, hc = get_params l1
+    and days = get_days l2 in
+      { hc = hc; dc=dc; days=days }
+
 
 (* SOLUTION ENTRYPOINT *)
 let () =
   let input_vals = get_inputVals in
       let prefixes = get_simplified_prefixes input_vals.days input_vals.hc in
-        Printf.printf "%d\n" (solve prefixes);
+      Printf.printf "%d\n" (solve prefixes);
 
--- project_1/part_1/solve.ml
+++ project_1/part_1/solve.ml
@@ -1,18 +1,50 @@
 (* LIBRARY FUNCTIONS *)
-let first lst = List.nth lst 0
-let second lst = List.nth lst 1
 let last lst = List.nth lst (List.length lst - 1)
-
-let rec sum lst = 
-  match lst with
-    | [] -> 0
-    | head::tail -> head + sum tail
-
-let sum2 lst = List.fold_left (+) 0 lst
-
 type inputVals = { hc:int; dc:int; days:int list }
-type impIdx = { idx:int; vl:int }
-let new_imp idx vl = { idx = idx; vl = vl }
+
+
+(* ALGORITHMS *)        
+let get_simplified_prefixes l hc =
+  let simplify v = -1 * (v + hc) in
+    let partial_apply acc v =
+      match acc with 
+      | [] -> [simplify v]
+      | [a] 
+      | a :: _ -> (simplify v + a) :: acc
+    in
+      0 :: List.rev (List.fold_left partial_apply [] l)
+      
+let solve prefixes =
+  if last prefixes >= 0 then (List.length prefixes - 1)
+  else 
+    let apply_left f acc v = 
+      match acc with
+      | [] -> [v]
+      | [a] 
+      | a :: _ -> (f a v) :: acc 
+    in 
+      let apply_right f v acc = apply_left f acc v in
+      let valid_left = List.rev (List.fold_left (apply_left min) [] prefixes )
+      and valid_right = List.fold_right (apply_right max) prefixes []
+      in
+        let rec rec_solve valid_left valid_right i j res =
+          match (valid_left, valid_right) with
+          | ([], _) -> res
+          | (_, []) -> res
+          | ([a], hd::tl) -> begin 
+              if a < hd 
+              then rec_solve valid_left tl i (j+1) (max res (j-i)) 
+              else res end
+          | (hd::tl, [a]) -> begin
+              if hd < a 
+              then max res (j-i)
+              else rec_solve tl valid_right (i+1) j res end
+          | (hd1::tl1, hd2::tl2) -> begin
+              if hd1<hd2
+              then rec_solve valid_left tl2 i (j+1) (max res (j-i))
+              else rec_solve tl1 valid_right (i+1) j res end
+        in
+          (rec_solve valid_left valid_right 0 0 (-1)) 
 
 
 (* ARGUMENT PARSING *)
@@ -30,8 +62,8 @@ let readfile fn =
       raise e
 
 let get_params line = 
-  let first_int vals = int_of_string (first vals)
-  and second_int vals = int_of_string (second vals)
+  let first_int vals = int_of_string (List.hd vals)
+  and second_int vals = int_of_string (List.nth vals 1)
   in
     let parse_args args = first_int args, second_int args 
     and values = String.split_on_char ' ' line
@@ -44,78 +76,16 @@ let get_days line =
     List.map int_of_string values
 
 let get_inputVals = 
-  let filename = Sys.argv.(1)
-  in
-    let l1,l2 = readfile filename
-    in 
-      let dc, hc = get_params l1
-      and days = get_days l2
-      in
-        { hc = hc; dc = dc; days = days }
-
-
-(* ALGORITHMS *)        
-let simplify_values values hc = 
-  let simplify v = -1 * (v + hc)
-  in 
-    List.map simplify values
-
-let get_prefixes l =
-  let rec inner_prefixes l last_sum res =
-    match l with
-    | [] -> []
-    | [a] ->  res @ [ a + last_sum ]
-    | hd::tl -> 
-      let new_sum = hd + last_sum 
-      and new_res = res @ [ hd + last_sum ]
-      in
-        inner_prefixes tl new_sum new_res
-        in
-          0 :: inner_prefixes l 0 [] 
-  
-let find_leftmost_idx idxs target =
-  let last_idx = List.length idxs - 1
-  in
-    let rec binary_search values target_value low high found =
-      if low<high then 
-        let mid = low + (high-low)/2 in
-          if target_value >= (List.nth values mid).vl 
-          then binary_search values target_value low (mid-1) mid
-          else binary_search values target_value (mid+1) high found
-      else (List.nth values found).idx
-    in 
-      binary_search idxs target 0 last_idx last_idx
-
-
-let solve prefixes =
-  let local_solve pfx pos idxs =
-    if pfx >= (last idxs).vl
-      then pos - find_leftmost_idx idxs pfx
-      else 0
-  in
-    let rec inner_solve prefixes imp_idxs pos res =
-      match prefixes with
-        | [] -> res
-        | [a] -> max res (local_solve a pos imp_idxs) 
-        | hd::tl -> 
-            let local_max = max res (local_solve hd pos imp_idxs)
-            and new_imp = if hd < (last imp_idxs).vl then [new_imp pos hd] else [] 
-            in
-              inner_solve tl (imp_idxs @ new_imp) (pos+1) local_max
-    in
-      inner_solve prefixes [new_imp 0 0] 0 0
+  let filename = Sys.argv.(1) in
+  let l1,l2 = readfile filename in
+    let dc, hc = get_params l1
+    and days = get_days l2 in
+      { hc = hc; dc=dc; days=days }
 
 
 (* SOLUTION ENTRYPOINT *)
 let () =
-  let input_vals = get_inputVals 
-  in
-    let simplified = simplify_values input_vals.days input_vals.hc
-    in
-      let prefixes = get_prefixes simplified
-      in
-        print_endline (string_of_int (solve prefixes));
-        (*
-        print_int (solve prefixes)
-        *)
+  let input_vals = get_inputVals in
+      let prefixes = get_simplified_prefixes input_vals.days input_vals.hc in
+      Printf.printf "%d\n" (solve prefixes);
 
