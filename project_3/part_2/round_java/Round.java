e9446e910276f50623202de3f3090eaf72b9ed83
kenji <trafalgar1618@hotmail.com>
2021-07-10 23:11:33 +0300

    Fucking sagonas

--- project_3/part_2/round_java/Round.java
+++ project_3/part_2/round_java/Round.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 
 public class Round {
   public static void main(String[] args) {
-    // Solve(args[0]);
-    SolveMultiple();
+    Solve(args[0]);
+    //SolveMultiple();
   }
 
   public static InputVals ParseFile(String filename) throws FileNotFoundException {
@@ -30,46 +30,50 @@ public class Round {
     return inputVals;
   }
 
-  public static int CalculateTownDistance(int town, InputVals inputVals) {
-    int current = -1;
-    int rest = 0;
+  public static int GetDistance(int townFrom, int townTo, int townCount) {
+    return townTo - townFrom >= 0 ? townTo - townFrom : townCount - (townFrom - townTo);
+  }
 
-    for (int i = 1; i < inputVals.TownCount; i++) {
-      int currentPosition = (town + i) % inputVals.TownCount;
+  public static TownDistance CalculateTown(InputVals iv) {
+    int maxDistance = GetDistance(iv.GetNextPopulatedTown(), 0, iv.TownCount);
 
-      if (inputVals.Positions.containsKey(currentPosition)) {
-        int currentDistance = (inputVals.TownCount - i) % inputVals.TownCount;
-        if (current == -1) {
-          current = currentDistance;
-          rest -= current;
-        }
-        rest += currentDistance * inputVals.Positions.get(currentPosition);
-      }
+    int totalDistance = 0;
+    for (Integer pos : iv.Positions.keySet()) {
+      totalDistance += GetDistance(pos, 0, iv.TownCount) * iv.Positions.get(pos);
     }
 
-    if (current - rest <= 1) {
-      return rest + current;
-    } else {
-      return -1;
-    }
+    return new TownDistance(0, maxDistance, totalDistance);
+  }
+
+  public static TownDistance CalculateTown(InputVals iv, TownDistance last) {
+    int currentTownId = last.TownId + 1;
+
+    int maxDistance =   iv.Positions.containsKey(currentTownId)
+    ? GetDistance(iv.GetNextPopulatedTown(), currentTownId, iv.TownCount)
+    : last.MaxDistance + 1;
+
+    int currentTownCarCount = iv.Positions.containsKey(currentTownId) ? iv.Positions.get(currentTownId) : 0;
+    int totalDistance = last.TotalDistance + iv.CarCount - iv.TownCount * currentTownCarCount;
+
+    return new TownDistance(currentTownId, maxDistance, totalDistance);
   }
 
   public static void Solve(String filename) {
     try {
       InputVals inputVals = ParseFile(filename);
 
-      Integer townResult = -1;
-      Integer distanceResult = -1;
+      TownDistance last = CalculateTown(inputVals);
+      TownDistance result = last.IsValid() ? last : null;
 
-      for (int i = 0; i < inputVals.TownCount; i++) {
-        int distance = CalculateTownDistance(i, inputVals);
-        if (distanceResult == -1 || (distance >= 0 && distance < distanceResult)) {
-          townResult = i;
-          distanceResult = distance;
+      for (int i = 1; i < inputVals.TownCount; i++) {
+        TownDistance current = CalculateTown(inputVals, last);
+        if (current.IsValid() && (result == null || current.TotalDistance < result.TotalDistance)) {
+          result = current;
         }
+        last = current;
       }
 
-      System.out.format("%d %d\n", distanceResult, townResult);
+      System.out.format("%d %d\n", result.TotalDistance, result.TownId);
 
     } catch (FileNotFoundException e) {
       e.printStackTrace();
@@ -78,11 +82,27 @@ public class Round {
 
   // TESTBED for all the testcases.
   public static void SolveMultiple() {
-    String filename_placeholder = "../../testcases/r@@.txt";
+    String filename_placeholder = "testcases/r@@.txt";
     for (int i = 1; i < 7; i++) {
       String filename = filename_placeholder.replace("@@", Integer.toString(i));
       Solve(filename);
     }
+  }
+}
+
+class TownDistance {
+  public int TownId;
+  public int MaxDistance;
+  public int TotalDistance;
+
+  public TownDistance(int townId, int maxDistance, int totalDistance) {
+    TownId = townId;
+    MaxDistance = maxDistance;
+    TotalDistance = totalDistance;
+  }
+
+  public boolean IsValid() {
+    return MaxDistance - (TotalDistance - MaxDistance) <= 1;
   }
 }
 
@@ -91,10 +111,14 @@ class InputVals {
   public int CarCount;
   public HashMap<Integer, Integer> Positions;
 
+  private ArrayList<Integer> _populatedTowns;
+  private int _currentPopulatedTownIndex;
+
   public InputVals(int townCount, int carCount, ArrayList<Integer> positions) {
     TownCount = townCount;
     CarCount = carCount;
     InitializePositions(positions);
+    InitializePopulatedTowns();
   }
 
   private void InitializePositions(ArrayList<Integer> positions) {
@@ -105,5 +129,25 @@ class InputVals {
       }
       Positions.put(pos, Positions.get(pos) + 1);
     }
+  }
+
+  private void InitializePopulatedTowns() {
+    _populatedTowns = new ArrayList<Integer>();
+    for (int i = 1; i < TownCount; i++) {
+      if (Positions.containsKey(i)) {
+        _populatedTowns.add(i);
+      }
+    }
+
+    if (Positions.containsKey(0)) {
+      _populatedTowns.add(0);
+    }
+
+    _currentPopulatedTownIndex = -1;
+  }
+
+  public int GetNextPopulatedTown() {
+    _currentPopulatedTownIndex = (_currentPopulatedTownIndex + 1) % _populatedTowns.size();
+    return _populatedTowns.get(_currentPopulatedTownIndex);
   }
 }
