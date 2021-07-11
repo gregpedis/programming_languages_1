be49d886d0c4fe1147c518d9c4a96ffcb7fb2f74
gmin96 <58821378+gmin96@users.noreply.github.com>
2021-07-11 18:08:55 +0300

    Some work on BFS

--- project_3/part_1/QSsort.java
+++ project_3/part_1/QSsort.java
@@ -1,42 +1,90 @@
-import java.util.Stack;
-import java.util.LinkedList;
+import java.util.Stack; // stack
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList; // list
+import java.util.LinkedList; // queue
 import java.util.List;
-import java.util.Objects;
+import java.util.Scanner;
+import java.util.HashSet;
 
 public class QSsort {
   public static void main(String[] args) {
-    //Solve(args[0]);
     SolveMultiple();
+   //  Solve(args[0]);
   }
 
   public static void Solve(String filename) {
-    var initial_state = ParseFile(filename);
-    var solution = ExecuteBFS(initial_state);
-    System.out.println(solution);
+    try {
+      State initial_state = ParseFile(filename);
+      String solution = ExecuteBFS(initial_state);
+      System.out.println(solution);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
   }
 
   // TESTBED for all the testcases.
   public static void SolveMultiple() {
-    String filename_placeholder = "../testcases/qs@@.txt";
+    String filename_placeholder = "project_3/testcases/qs@@.txt";
     for (int i = 1; i < 7; i++) {
-      var filename = filename_placeholder.replace("@@", Integer.toString(i));
+      String filename = filename_placeholder.replace("@@", Integer.toString(i));
       Solve(filename);
     }
   }
 
   public static String ExecuteBFS(State initial) {
-    // initialize a set of ints
-    // initialize a queue of states
-    // while-loop over it and do a bfs
-    // when a state IsDone() then return the GetOperationsString()
-    return "placeholder";
+    HashSet<Integer> visited = new HashSet<Integer>();
+    LinkedList<State> stateQueue = new LinkedList<State>();
+
+    visited.add(initial.hashCode());
+    stateQueue.add(initial);
+
+    while (stateQueue.size() > 0) {
+      State current = stateQueue.remove();
+
+      if (current.IsDone()) {
+        return current.GetOperationsString();
+      }
+
+      if (current.OperationAllowedQ()) {
+        State nextStateQ = current.ExecuteOperationQ();
+        if (!visited.contains(nextStateQ.hashCode())) {
+          visited.add(nextStateQ.hashCode());
+          stateQueue.add(nextStateQ);
+        }
+      }
+
+      if (current.OperationAllowedS()) {
+        State nextStateS = current.ExecuteOperationS();
+        if (!visited.contains(nextStateS.hashCode())) {
+          visited.add(nextStateS.hashCode());
+          stateQueue.add(nextStateS);
+        }
+      }
+
+      
+      }  return "what the fuck";
+    
   }
 
-  public static State ParseFile(String filename) {
-    //read the file
-    // create an initial state
-    // return initial state
-    return null;
+  public static State ParseFile(String filename) throws FileNotFoundException {
+    File myfile = new File(filename);
+    Scanner scanner = new Scanner(myfile);
+    ArrayList<Integer> mylist = new ArrayList<Integer>();
+
+    scanner.nextLine();
+    String[] tokens = scanner.nextLine().split(" ");
+    for (String token : tokens) {
+
+      int entry = Integer.parseInt(token);
+      mylist.add(entry);
+    }
+
+    LinkedList<Integer> q = new LinkedList<Integer>(mylist);
+    Stack<Integer> s = new Stack<Integer>();
+
+    scanner.close();
+    return new State("", q, s);
   }
 }
 
@@ -60,12 +108,9 @@ class State {
     return _stack.isEmpty() && isSorted(_queue);
   }
 
-  private boolean isSorted(List<Integer> collection) 
-  {
-    for (int i = 0; i < collection.size() - 1; i++) 
-    {
-      if (collection.get(i) > collection.get(i + 1)) 
-      {
+  private boolean isSorted(List<Integer> collection) {
+    for (int i = 0; i < collection.size() - 1; i++) {
+      if (collection.get(i) > collection.get(i + 1)) {
         return false;
       }
     }
@@ -81,20 +126,20 @@ class State {
   }
 
   public State ExecuteOperationQ() {
-    var new_queue = new LinkedList<Integer>(_queue);
-    var new_stack = (Stack<Integer>) _stack.clone();
+    LinkedList<Integer> new_queue = new LinkedList<Integer>(_queue);
+    Stack<Integer> new_stack = (Stack<Integer>) _stack.clone();
 
-    new_stack.push(new_queue.pop());
-    var new_operation_string = _operationsString + "Q";
+    new_stack.push(new_queue.remove());
+    String new_operation_string = _operationsString + "Q";
 
     return new State(new_operation_string, new_queue, new_stack);
   }
 
   public State ExecuteOperationS() {
-    var new_queue = new LinkedList<Integer>(_queue);
-    var new_stack = (Stack<Integer>) _stack.clone();
-    new_queue.push(new_stack.pop());
-    var new_operation_string = _operationsString + "S";
+    LinkedList<Integer> new_queue = new LinkedList<Integer>(_queue);
+    Stack<Integer> new_stack = (Stack<Integer>) _stack.clone();
+    new_queue.add(new_stack.pop());
+    String new_operation_string = _operationsString + "S";
     return new State(new_operation_string, new_queue, new_stack);
   }
 
