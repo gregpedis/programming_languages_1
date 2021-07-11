20062501a70d89bdcd63e8d20bbbf492eb5a5b61
kenji <trafalgar1618@hotmail.com>
2021-07-11 17:34:39 +0300

    Added comment on java

--- project_3/part_2/round_java/Round.java
+++ project_3/part_2/round_java/Round.java
@@ -7,9 +7,10 @@ import java.util.Scanner;
 public class Round {
   public static void main(String[] args) {
     Solve(args[0]);
-    //SolveMultiple();
+    // SolveMultiple();
   }
 
+  // Reads the input from a file and creates an InputVals instance.
   public static InputVals ParseFile(String filename) throws FileNotFoundException {
     File file = new File(filename);
     Scanner scanner = new Scanner(file);
@@ -30,27 +31,35 @@ public class Round {
     return inputVals;
   }
 
+  // Calculates the distance between townFrom and townTo, only moving in one
+  // direction.
   public static int GetDistance(int townFrom, int townTo, int townCount) {
     return townTo - townFrom >= 0 ? townTo - townFrom : townCount - (townFrom - townTo);
   }
 
+  // Calculates the TownDistance instance for the town with index 0.
   public static TownDistance CalculateTown(InputVals iv) {
     int maxDistance = GetDistance(iv.GetNextPopulatedTown(), 0, iv.TownCount);
 
     int totalDistance = 0;
     for (Integer pos : iv.Positions.keySet()) {
+      // Since positions is a frequency table, calculate distance times the frequency
+      // of the town.
       totalDistance += GetDistance(pos, 0, iv.TownCount) * iv.Positions.get(pos);
     }
 
     return new TownDistance(0, maxDistance, totalDistance);
   }
 
+  // Calculates the TownDistance instance for town last+1.
   public static TownDistance CalculateTown(InputVals iv, TownDistance last) {
     int currentTownId = last.TownId + 1;
 
-    int maxDistance =   iv.Positions.containsKey(currentTownId)
-    ? GetDistance(iv.GetNextPopulatedTown(), currentTownId, iv.TownCount)
-    : last.MaxDistance + 1;
+    // If this town has cars, get distance from the next populated town.
+    // Else, use last max distance plus one.
+    int maxDistance = iv.Positions.containsKey(currentTownId)
+        ? GetDistance(iv.GetNextPopulatedTown(), currentTownId, iv.TownCount)
+        : last.MaxDistance + 1;
 
     int currentTownCarCount = iv.Positions.containsKey(currentTownId) ? iv.Positions.get(currentTownId) : 0;
     int totalDistance = last.TotalDistance + iv.CarCount - iv.TownCount * currentTownCarCount;
@@ -58,13 +67,17 @@ public class Round {
     return new TownDistance(currentTownId, maxDistance, totalDistance);
   }
 
+  // Solution entry point.
   public static void Solve(String filename) {
     try {
       InputVals inputVals = ParseFile(filename);
 
+      // Calculate the TownDistance with index 0.
       TownDistance last = CalculateTown(inputVals);
       TownDistance result = last.IsValid() ? last : null;
 
+      // Loop through the towns 1 to N-1 and calculate the TownDistance for each.
+      // Update gradually the solution.
       for (int i = 1; i < inputVals.TownCount; i++) {
         TownDistance current = CalculateTown(inputVals, last);
         if (current.IsValid() && (result == null || current.TotalDistance < result.TotalDistance)) {
@@ -90,6 +103,7 @@ public class Round {
   }
 }
 
+// This class is a container for a town, containing the max and total distance for this town.
 class TownDistance {
   public int TownId;
   public int MaxDistance;
@@ -101,6 +115,8 @@ class TownDistance {
     TotalDistance = totalDistance;
   }
 
+  // A town is considered valid if the max distance is at most 1 more than 
+  // the sum of the rest of the distances.
   public boolean IsValid() {
     return MaxDistance - (TotalDistance - MaxDistance) <= 1;
   }
@@ -121,6 +137,8 @@ class InputVals {
     InitializePopulatedTowns();
   }
 
+  // Frequency table to calculate the total distance of a town,
+  // while also be able to do [containsKey] and check if a town has cars in O(1).
   private void InitializePositions(ArrayList<Integer> positions) {
     Positions = new HashMap<Integer, Integer>();
     for (Integer pos : positions) {
@@ -131,6 +149,8 @@ class InputVals {
     }
   }
 
+  // An arraylist that contains all the towns that have cars, which goes 1..N,0
+  // Also initialize an index as -1.
   private void InitializePopulatedTowns() {
     _populatedTowns = new ArrayList<Integer>();
     for (int i = 1; i < TownCount; i++) {
@@ -146,6 +166,7 @@ class InputVals {
     _currentPopulatedTownIndex = -1;
   }
 
+  // Consumes the arraylist as a sudo-queue, "bumbing" the currentIndex to the next one, in a cyclic manner.
   public int GetNextPopulatedTown() {
     _currentPopulatedTownIndex = (_currentPopulatedTownIndex + 1) % _populatedTowns.size();
     return _populatedTowns.get(_currentPopulatedTownIndex);
